8f88fc5ec0031600ba673bdf7091241334d17545
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-07-31 16:58:24 +0300

    Initial import

---
+++ include/private/plugins/trigger.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2021 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2021 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-trigger
+ * Created on: 31 июл. 2021 г.
+ *
+ * lsp-plugins-trigger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-trigger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-trigger. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_PLUGINS_TRIGGER_H_
+#define PRIVATE_PLUGINS_TRIGGER_H_
+
+
+
+#endif /* PRIVATE_PLUGINS_TRIGGER_H_ */

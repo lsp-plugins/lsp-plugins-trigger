7e56a159d2e709bbdb4ea92e3178b731e961bc6c
sadko4u <sadko4u@gmail.com>
2022-01-16 01:44:05 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-trigger.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_TRIGGER
-ARTIFACT_NAME               = lsp-plugins-trigger
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Trigger Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.3-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_TRIGGER
+PLUGIN_PACKAGE_NAME     = lsp-plugins

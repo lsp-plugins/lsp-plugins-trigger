d5973e291020893c09a48ff979fa7d313378eb58
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-07-10 22:31:46 +0300

    C++ 11 support

--- include/private/meta/trigger.h
+++ include/private/meta/trigger.h
@@ -33,83 +33,83 @@ namespace lsp
         // Sampler metadata
         struct trigger_metadata
         {
-            static const float SAMPLE_PITCH_MIN         = -24.0f;   // Minimum pitch (st)
-            static const float SAMPLE_PITCH_MAX         = 24.0f;    // Maximum pitch (st)
-            static const float SAMPLE_PITCH_DFL         = 0.0f;     // Pitch (st)
-            static const float SAMPLE_PITCH_STEP        = 0.01f;    // Pitch step (st)
+            static constexpr float SAMPLE_PITCH_MIN         = -24.0f;   // Minimum pitch (st)
+            static constexpr float SAMPLE_PITCH_MAX         = 24.0f;    // Maximum pitch (st)
+            static constexpr float SAMPLE_PITCH_DFL         = 0.0f;     // Pitch (st)
+            static constexpr float SAMPLE_PITCH_STEP        = 0.01f;    // Pitch step (st)
 
-            static const float SAMPLE_LENGTH_MIN        = 0.0f;     // Minimum length (ms)
-            static const float SAMPLE_LENGTH_MAX        = 64000.0f; // Maximum sample length (ms)
-            static const float SAMPLE_LENGTH_DFL        = 0.0f;     // Sample length (ms)
-            static const float SAMPLE_LENGTH_STEP       = 0.1f;     // Sample step (ms)
+            static constexpr float SAMPLE_LENGTH_MIN        = 0.0f;     // Minimum length (ms)
+            static constexpr float SAMPLE_LENGTH_MAX        = 64000.0f; // Maximum sample length (ms)
+            static constexpr float SAMPLE_LENGTH_DFL        = 0.0f;     // Sample length (ms)
+            static constexpr float SAMPLE_LENGTH_STEP       = 0.1f;     // Sample step (ms)
 
-            static const float PREDELAY_MIN             = 0.0f;     // Pre-delay min (ms)
-            static const float PREDELAY_MAX             = 100.0f;   // Pre-delay max (ms)
-            static const float PREDELAY_DFL             = 0.0f;     // Pre-delay default (ms)
-            static const float PREDELAY_STEP            = 0.1f;     // Pre-delay step (ms)
+            static constexpr float PREDELAY_MIN             = 0.0f;     // Pre-delay min (ms)
+            static constexpr float PREDELAY_MAX             = 100.0f;   // Pre-delay max (ms)
+            static constexpr float PREDELAY_DFL             = 0.0f;     // Pre-delay default (ms)
+            static constexpr float PREDELAY_STEP            = 0.1f;     // Pre-delay step (ms)
 
-            static const size_t MESH_SIZE               = 320;      // Maximum mesh size
-            static const size_t TRACKS_MAX              = 2;        // Maximum tracks per mesh/sample
-            static const size_t SAMPLE_FILES            = 8;        // Number of sample files per trigger
-            static const size_t BUFFER_SIZE             = 4096;     // Size of temporary buffer
-            static const size_t PLAYBACKS_MAX           = 8192;     // Maximum number of simultaneously playing samples
-            static const float ACTIVITY_LIGHTING        = 0.1f;     // Activity lighting (seconds)
+            static constexpr size_t MESH_SIZE               = 320;      // Maximum mesh size
+            static constexpr size_t TRACKS_MAX              = 2;        // Maximum tracks per mesh/sample
+            static constexpr size_t SAMPLE_FILES            = 8;        // Number of sample files per trigger
+            static constexpr size_t BUFFER_SIZE             = 4096;     // Size of temporary buffer
+            static constexpr size_t PLAYBACKS_MAX           = 8192;     // Maximum number of simultaneously playing samples
+            static constexpr float ACTIVITY_LIGHTING        = 0.1f;     // Activity lighting (seconds)
 
-            static const float  DETECT_LEVEL_DFL        = GAIN_AMP_M_12_DB;     // Default detection level [G]
+            static constexpr float  DETECT_LEVEL_DFL        = GAIN_AMP_M_12_DB;     // Default detection level [G]
 
-            static const float  RELEASE_LEVEL_MIN       = 0.0f;     // Minimum relative release level
-            static const float  RELEASE_LEVEL_DFL       = GAIN_AMP_M_3_DB;      // Default release level [G]
-            static const float  RELEASE_LEVEL_MAX       = 0.0f;     // Maximum relative release level
-            static const float  RELEASE_LEVEL_STEP      = 0.0001f;  // Release level step [G]
+            static constexpr float  RELEASE_LEVEL_MIN       = 0.0f;     // Minimum relative release level
+            static constexpr float  RELEASE_LEVEL_DFL       = GAIN_AMP_M_3_DB;      // Default release level [G]
+            static constexpr float  RELEASE_LEVEL_MAX       = 0.0f;     // Maximum relative release level
+            static constexpr float  RELEASE_LEVEL_STEP      = 0.0001f;  // Release level step [G]
 
-            static const float  DETECT_TIME_MIN         = 0.0f;     // Minimum detection time [ms]
-            static const float  DETECT_TIME_DFL         = 5.0f;     // Default detection time [ms]
-            static const float  DETECT_TIME_MAX         = 20.0f;    // Maximum detection time [ms]
-            static const float  DETECT_TIME_STEP        = 0.0025f;  // Detection time step [ms]
+            static constexpr float  DETECT_TIME_MIN         = 0.0f;     // Minimum detection time [ms]
+            static constexpr float  DETECT_TIME_DFL         = 5.0f;     // Default detection time [ms]
+            static constexpr float  DETECT_TIME_MAX         = 20.0f;    // Maximum detection time [ms]
+            static constexpr float  DETECT_TIME_STEP        = 0.0025f;  // Detection time step [ms]
 
-            static const float  RELEASE_TIME_MIN        = 0.0f;     // Minimum release time [ms]
-            static const float  RELEASE_TIME_DFL        = 10.0f;    // Default release time [ms]
-            static const float  RELEASE_TIME_MAX        = 100.0f;   // Maximum release time [ms]
-            static const float  RELEASE_TIME_STEP       = 0.005f;   // Release time step [ms]
+            static constexpr float  RELEASE_TIME_MIN        = 0.0f;     // Minimum release time [ms]
+            static constexpr float  RELEASE_TIME_DFL        = 10.0f;    // Default release time [ms]
+            static constexpr float  RELEASE_TIME_MAX        = 100.0f;   // Maximum release time [ms]
+            static constexpr float  RELEASE_TIME_STEP       = 0.005f;   // Release time step [ms]
 
-            static const float  DYNAMICS_MIN            = 0.0f;     // Minimum dynamics [%]
-            static const float  DYNAMICS_DFL            = 10.0f;    // Default dynamics [%]
-            static const float  DYNAMICS_MAX            = 100.0f;   // Maximum dynamics [%]
-            static const float  DYNAMICS_STEP           = 0.05f;    // Dynamics step [%]
+            static constexpr float  DYNAMICS_MIN            = 0.0f;     // Minimum dynamics [%]
+            static constexpr float  DYNAMICS_DFL            = 10.0f;    // Default dynamics [%]
+            static constexpr float  DYNAMICS_MAX            = 100.0f;   // Maximum dynamics [%]
+            static constexpr float  DYNAMICS_STEP           = 0.05f;    // Dynamics step [%]
 
-            static const float  REACTIVITY_MIN          = 0.000;    // Minimum reactivity [ms]
-            static const float  REACTIVITY_MAX          = 250;      // Maximum reactivity [ms]
-            static const float  REACTIVITY_DFL          = 20;       // Default reactivity [ms]
-            static const float  REACTIVITY_STEP         = 0.01;     // Reactivity step
+            static constexpr float  REACTIVITY_MIN          = 0.000;    // Minimum reactivity [ms]
+            static constexpr float  REACTIVITY_MAX          = 250;      // Maximum reactivity [ms]
+            static constexpr float  REACTIVITY_DFL          = 20;       // Default reactivity [ms]
+            static constexpr float  REACTIVITY_STEP         = 0.01;     // Reactivity step
 
-            static const float  HISTORY_TIME            = 5.0f;     // Amount of time to display history [s]
-            static const size_t HISTORY_MESH_SIZE       = 640;      // 640 dots for history
+            static constexpr float  HISTORY_TIME            = 5.0f;     // Amount of time to display history [s]
+            static constexpr size_t HISTORY_MESH_SIZE       = 640;      // 640 dots for history
 
-            static const float  HPF_MIN                 = 10.0f;
-            static const float  HPF_MAX                 = 20000.0f;
-            static const float  HPF_DFL                 = 10.0f;
-            static const float  HPF_STEP                = 0.0025f;
+            static constexpr float  HPF_MIN                 = 10.0f;
+            static constexpr float  HPF_MAX                 = 20000.0f;
+            static constexpr float  HPF_DFL                 = 10.0f;
+            static constexpr float  HPF_STEP                = 0.0025f;
 
-            static const float  LPF_MIN                 = 10.0f;
-            static const float  LPF_MAX                 = 20000.0f;
-            static const float  LPF_DFL                 = 20000.0f;
-            static const float  LPF_STEP                = 0.0025f;
+            static constexpr float  LPF_MIN                 = 10.0f;
+            static constexpr float  LPF_MAX                 = 20000.0f;
+            static constexpr float  LPF_DFL                 = 20000.0f;
+            static constexpr float  LPF_STEP                = 0.0025f;
 
-            static const float DRIFT_MIN                = 0.0f;     // Minimum delay
-            static const float DRIFT_DFL                = 0.0f;     // Default delay
-            static const float DRIFT_STEP               = 0.1f;     // Delay step
-            static const float DRIFT_MAX                = 100.0f;   // Maximum delay
+            static constexpr float DRIFT_MIN                = 0.0f;     // Minimum delay
+            static constexpr float DRIFT_DFL                = 0.0f;     // Default delay
+            static constexpr float DRIFT_STEP               = 0.1f;     // Delay step
+            static constexpr float DRIFT_MAX                = 100.0f;   // Maximum delay
 
-            static const float DYNA_MIN                 = 0.0f;     // Minimum dynamics
-            static const float DYNA_DFL                 = 0.0f;     // Default dynamics
-            static const float DYNA_STEP                = 0.05f;    // Dynamics step
-            static const float DYNA_MAX                 = 100.0f;   // Maximum dynamics
+            static constexpr float DYNA_MIN                 = 0.0f;     // Minimum dynamics
+            static constexpr float DYNA_DFL                 = 0.0f;     // Default dynamics
+            static constexpr float DYNA_STEP                = 0.05f;    // Dynamics step
+            static constexpr float DYNA_MAX                 = 100.0f;   // Maximum dynamics
 
-            static const size_t MODE_DFL                = 1;        // RMS
+            static constexpr size_t MODE_DFL                = 1;        // RMS
 
-            static const size_t MIDI_CHANNEL_DFL        = 0;        // Default channel
-            static const size_t MIDI_NOTE_DFL           = 11;       // B
-            static const size_t MIDI_OCTAVE_DFL         = 2;        // 2nd octave
+            static constexpr size_t MIDI_CHANNEL_DFL        = 0;        // Default channel
+            static constexpr size_t MIDI_NOTE_DFL           = 11;       // B
+            static constexpr size_t MIDI_OCTAVE_DFL         = 2;        // 2nd octave
         };
 
         // Different samplers

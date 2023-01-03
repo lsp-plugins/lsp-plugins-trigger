f049025ba3181f16c499c263d85a53a4f6e1555d
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:49:45 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-trigger
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Trigger Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.8-devel
 
 
